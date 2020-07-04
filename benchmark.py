c3336be7c440a910d0605f3a6040855ef4197bf7
Georgi Ganchev <g.y.ganchev@gmail.com>
2020-07-04 18:21:32 +0300

    BinaryTree code cleaning

---
+++ benchmark.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 12:03:48 2020
+
+@author: GeorgiGanchev
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+
+titanic = pd.read_csv('titanic.txt')
+X = titanic[['Pclass',  'Sex', 'Age', 'SibSp',
+        'Parch', 'Fare', 'Cabin', 'Embarked']]
+y = titanic['Survived']
+
+X['Fare'] = X['Fare'].astype(float).fillna(X['Fare'].astype(float).mean())
+X['Age'] = X['Age'].astype(float).fillna(X['Age'].astype(float).mean())
+X = X.fillna('MISSING')
+
+X_cl = X.astype('category')
+X_cl['Fare'] = X_cl['Fare'].astype(float)
+X_cl['Age'] = X_cl['Age'].astype(float)
+X_cl.dtypes
+
+
+X_sk = pd.get_dummies(X)
+
+cl_perf = []
+sk_perf = []
+for i in range(1,15):
+    print(i)
+    cl_tree = BinaryClassificationTree(max_depth=i)
+    cl_tree.fit(X_cl,y)
+    pred = cl_tree.predict(X_cl)
+    #print('accuracy cl:' ,np.mean(pred == y))
+    
+    clf = DecisionTreeClassifier(max_depth=i)
+    clf.fit(X_sk,y)
+    pred_sk  = clf.predict(X_sk)
+    #print('accuracy sk:' ,np.mean(pred_sk == y))
+    cl_perf.extend([np.mean(pred == y)])
+    sk_perf.extend([np.mean(pred_sk == y)])
+    
+plt.plot(range(1,15),cl_perf,label= 'Categorical CART')
+plt.plot(range(1,15),sk_perf,label='SKlearn CART ')
+plt.title('TITCANIC - SKlearn CART vs Categorical Cart - CONVERGENCE')
+plt.xlabel('max_depth')
+plt.ylabel('Accurcay')
+plt.legend()
+
+
+
+german_credit = pd.read_csv('german_credit.csv')
+
+X = german_credit.drop(columns='default')
+categorical_vars = list(X.dtypes[X.dtypes=='object'].index)
+X_sk = pd.get_dummies(X[categorical_vars])
+
+X_cl =X[categorical_vars].astype('category')
+
+y = german_credit['default']
+
+
+
+
+cl_perf = []
+sk_perf = []
+for i in range(1,20):
+    print(i)
+    cl_tree = BinaryClassificationTree(max_depth=i)
+    cl_tree.fit(X_cl,y)
+    #pred = cl_tree.predict(X_cl)
+    pred = cl_tree.predict_proba(X_cl)
+    #print('accuracy cl:' ,np.mean(pred == y))
+    
+    clf = DecisionTreeClassifier(max_depth=i)
+    clf.fit(X_sk,y)
+    #pred_sk  = clf.predict(X_sk)
+    pred_sk  = clf.predict_proba(X_sk)
+    #print('accuracy sk:' ,np.mean(pred_sk == y))
+    #cl_perf.extend([np.mean(pred == y)])
+    #sk_perf.extend([np.mean(pred_sk == y)])
+    cl_perf.extend([roc_auc_score(y,np.array(pred)[:,1]  )])
+    sk_perf.extend([roc_auc_score(y, pred_sk[:,1] )])
+    
+    
+plt.plot(range(1,20),cl_perf,label= 'Categorical CART')
+plt.plot(range(1,20),sk_perf,label='SKlearn CART ')
+plt.title('German Credit - SKlearn CART vs Categorical Cart - CONVERGENCE')
+plt.xlabel('max_depth')
+plt.ylabel('Accurcay')
+plt.legend()
---
+++ classification_tree.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 16:06:06 2020
+
+@author: GeorgiGanchev
+"""
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 16:56:46 2020
+
+@author: GeorgiGanchev
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.api.types import is_datetime64_any_dtype
+from pandas.api.types import is_bool_dtype
+from pandas.api.types import is_categorical_dtype
+from pandas.api.types import is_numeric_dtype
+from pandas.api.types import is_object_dtype
+from pandas.api.types import is_string_dtype
+
+
+
+import numpy as np
+
+
+class Node:
+    def __init__(self, predicted_class,probability,node_type='leaf'):
+        self.predicted_class = predicted_class
+        self.feature_index = 0
+        self.threshold = 0
+        self.left = None
+        self.right = None
+        self.node_type = node_type
+        self.probability = probability
+
+
+class BinaryClassificationTree:
+    def __init__(self, max_depth=float("inf")):
+        self.max_depth = max_depth
+
+    def fit(self, X, y):
+        self.n_classes_ = len(set(y))
+        X,y = self._prep_data(X,y)
+        #print(X[0:5,:])
+        self.tree_ = self._grow_tree(X, y)
+        
+
+    def predict(self, X):
+        X = X[self.feature_names]
+        X = X.values
+        return [self._predict(inputs) for inputs in X]
+    def predict_proba(self, X):
+        X = X[self.feature_names]
+        X = X.values
+        return [self._predict(inputs,ptype='prob') for inputs in X]
+    
+    
+    def _prep_data(self,X,y):
+        self.feature_names = X.columns
+        self.classes = np.unique(y)
+        self.numeric_features = self._get_dtype(X,'numeric')
+        self.string_features = self._get_dtype(X,'string')
+        self.categorical_features = self._get_dtype(X,'categorical')
+        self.date_features = self._get_dtype(X,'date')
+        self.bool_features = self._get_dtype(X,'bool')
+        self.feature_map = dict(zip(self.feature_names ,range(len(self.feature_names ))))
+        self.rev_feature_map = dict(zip(self.feature_map.values(),self.feature_map.keys()))
+        self.taget_map = dict(zip(self.classes ,range(len(self.classes ))))
+        self.rev_taget_map = dict(zip(self.taget_map.values(),self.taget_map.keys()))
+        self.feature_value_maps = {}
+        return X.values,y.map(self.taget_map )
+        
+
+    def _get_dtype(self,X,d_type):
+        dtype_func = {'numeric':is_numeric_dtype,
+                      'string':is_string_dtype,
+                      'categorical':is_categorical_dtype,
+                      'date':is_datetime64_any_dtype,
+                      'bool':is_bool_dtype}
+        cols = []
+        for f in X.columns:
+            if dtype_func[d_type](X[f]):
+                cols.extend([f])
+        return cols
+    def _format_categorical(self,X,y,idx):
+        df = pd.DataFrame(X[:,idx],columns = [idx])
+        df['y'] = y
+        df_grp =df.groupby([idx]).y.mean().sort_values()
+        self.feature_value_maps[idx] = {'map':dict(zip(df_grp.index,range(len(df_grp.index)))),
+                                        'rev_map':dict(zip(range(len(df_grp.index)),df_grp.index))}
+        return df[idx].map(self.feature_value_maps[idx]['map']).values
+    
+    def _format_feature(self,X,y,idx):
+        if self.rev_feature_map[idx] in self.numeric_features:
+            return X[:, idx], 'numeric'
+        elif self.rev_feature_map[idx] in self.categorical_features:
+            return self._format_categorical(X,y,idx) ,'categorical'
+        else:
+            raise ValueError("This tree supports only numeric or categorical features") 
+        
+    def _best_split(self, X, y):
+        m = y.size
+        if m <= 1:
+            return None, None,None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes_)]
+        best_gini = 1.0 - sum((n / m) ** 2 for n in num_parent)
+        best_idx, best_thr ,best_split_type = None, None,None
+        #print('initial gini: ',best_gini)
+        for idx in self.feature_names.map(self.feature_map):
+            xf,split_type = self._format_feature(X,y,idx)
+            thresholds, classes = zip(*sorted(zip(xf, y)))
+            num_left = [0] * self.n_classes_
+            num_right = num_parent.copy()
+            for i in range(1, m):
+                c = classes[i - 1]
+                num_left[c] += 1
+                num_right[c] -= 1
+                gini_left = 1.0 - sum(
+                    (num_left[x] / i) ** 2 for x in range(self.n_classes_)
+                )
+                gini_right = 1.0 - sum(
+                    (num_right[x] / (m - i)) ** 2 for x in range(self.n_classes_)
+                )
+                gini = (i * gini_left + (m - i) * gini_right) / m
+                if thresholds[i] == thresholds[i - 1]:
+                    continue
+                if gini < best_gini:
+                    best_gini = gini
+                    best_idx = idx
+                    best_split_type = split_type
+                    best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+            
+        return best_idx, best_thr ,best_split_type
+
+    def _grow_tree(self, X, y, depth=0):
+        num_samples_per_class = [np.sum(y == i) for i in range(self.n_classes_)]
+        predicted_class = np.argmax(num_samples_per_class)
+        prob = num_samples_per_class[predicted_class]/np.sum(num_samples_per_class)
+        node = Node(predicted_class=self.rev_taget_map[predicted_class],probability=prob)
+        if depth < self.max_depth:
+            #print('depth: ',depth)
+            idx, thr,split_type = self._best_split(X, y)            
+            #print(idx,split_type)
+            if idx is not None:
+                #indices_left = X[:, idx] < thr
+                var ,_  = self._format_feature(X,y,idx) 
+                #print(var)
+                indices_left = var < thr
+                X_left, y_left = X[indices_left], y[indices_left]
+                X_right, y_right = X[~indices_left], y[~indices_left]
+                node.feature_index = idx
+                if split_type == 'numeric':
+                    node.threshold = thr
+                    node.node_type = 'numeric'
+                else :
+                    #print(split_type)
+                    #print(idx)
+                    #print(thr)
+                    #print(self.feature_value_maps[idx]['rev_map'].values())
+                    thr_cat = pd.Series([x for x in self.feature_value_maps[idx]['rev_map'].keys() if x < thr])
+                    node.threshold = thr_cat.map(self.feature_value_maps[idx]['rev_map']).values
+                    node.node_type = 'categorical'
+                node.left = self._grow_tree(X_left, y_left, depth + 1)
+                node.right = self._grow_tree(X_right, y_right, depth + 1)
+        return node
+
+    def _predict(self, inputs,ptype = 'class'):
+        node = self.tree_
+        while node.left:
+            if node.node_type != 'categorical':
+                if inputs[node.feature_index] < node.threshold:
+                    node = node.left
+                else:
+                    node = node.right
+            else:
+                if inputs[node.feature_index] in node.threshold:
+                    
+                    node = node.left
+                else:
+                    node = node.right
+        if ptype == 'class':
+            return node.predicted_class
+        elif ptype == 'prob' :
+            if node.predicted_class == self.classes[0]:
+                return [node.probability,1- node.probability]
+            else:
+                return [1- node.probability,node.probability,]
+        else:
+            return np.nan
+
+
+# =============================================================================
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.datasets import load_iris
+# 
+# dataset = load_iris()
+# X, y = dataset.data, dataset.target
+# X = X[y<2,:]
+# y = y[y<2]
+# X = X[:,0:2]
+# X = pd.DataFrame(X)
+# X['good_cat'] = pd.Series(['A' if x ==1 else 'B' for x in  y ],dtype='category')
+# #X = X[['good_cat']]
+# y = pd.Series(y)
+# y = y.map({0:'HI',1:'BABY'})
+# X 
+# 
+# tree = BinaryClassificationTree(max_depth=float('inf'))
+# tree.fit(X,y)
+# pred = tree.predict(X)
+# print('accuracy my:' ,np.mean(pred == y))
+# 
+# tree.predict_proba(X)
+# 
+# tree.tree_.node_type
+# tree.tree_.left.node_type
+# tree.tree_.right.node_type
+# 
+# 
+# titanic = pd.read_csv('titanic.txt')
+# X = titanic[['Pclass',  'Sex', 'Age', 'SibSp',
+#        'Parch', 'Fare', 'Cabin', 'Embarked']]
+# y = titanic['Survived']
+# 
+# X['Fare'] = X['Fare'].astype(float).fillna(X['Fare'].astype(float).mean())
+# X['Age'] = X['Age'].astype(float).fillna(X['Age'].astype(float).mean())
+# X = X.fillna('MISSING')
+# 
+# X_my = X.astype('category')
+# X_my['Fare'] = X_my['Fare'].astype(float)
+# X_my['Age'] = X_my['Age'].astype(float)
+# X_my.dtypes
+# 
+# 
+# X_sk = pd.get_dummies(X)
+# 
+# my_perf = []
+# sk_perf = []
+# for i in range(1,15):
+#     print(i)
+#     my_tree = BinaryClassificationTree(max_depth=i)
+#     my_tree.fit(X_my,y)
+#     pred = my_tree.predict(X_my)
+#     #print('accuracy my:' ,np.mean(pred == y))
+#     
+#     clf = DecisionTreeClassifier(max_depth=i)
+#     clf.fit(X_sk,y)
+#     pred_sk  = clf.predict(X_sk)
+#     #print('accuracy sk:' ,np.mean(pred_sk == y))
+#     my_perf.extend([np.mean(pred == y)])
+#     sk_perf.extend([np.mean(pred_sk == y)])
+#     
+# plt.plot(range(1,15),my_perf,label= 'Categorical CART')
+# plt.plot(range(1,15),sk_perf,label='SKlearn CART ')
+# plt.title('SKlearn CART vs Categorical Cart - CONVERGENCE')
+# plt.xlabel('max_depth')
+# plt.ylabel('accurcay')
+# plt.legend()
+# =============================================================================
+
+
+
+# =============================================================================
+# clf = DecisionTreeClassifier(max_depth=None,min_samples_split=2)
+# clf.fit(X,y)
+# pred_sk  = clf.predict(X)
+# 
+# 
+# 
+# print('my vs sk: ', np.mean(pred == pred_sk))
+# print('accuracy my:' ,np.mean(pred == y))
+# print('accuracy sk:' ,np.mean(pred_sk == y))
+# #pd.crosstab(np.array(pred),y.values)
+# =============================================================================
+
+
+
+# =============================================================================
+# def vc(series):
+#     unique, counts  = np.unique(series.values, return_counts=True)
+#     return  dict(zip(unique, counts))
+# 
+# %timeit vc(df['target'])
+# %timeit df['target'].value_counts()
+# 
+# 
+# %timeit np.unique(df['target'].values)
+# %timeit df['target'].unique()
+# =============================================================================
+
+#df = pd.DataFrame({'x':np.random.choice(["A",'B','C','D'],100),
+#                   'y' : np.random.choice([0,1],100)})
+#
+#df.groupby(['x']).y.mean().sort_values()
